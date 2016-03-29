dc468ef46ce13a5755c1a2def351aa5449e82b61
FlorianFrank <ffrank@hm.edu>
2016-03-29 12:32:47 +0200

    JavaDoc
    Javadokumentation added

--- SplitString/src/DualSplitString.java
+++ SplitString/src/DualSplitString.java
@@ -1,15 +1,35 @@
 /**
- * Created by florianfrank on 29.03.16.
+ *  This class represents a string with two cursorPositions.
+ * The defaultposition of the first cursor is before the first lettern the position of the second cursor
+ * is after the last letter of the string.
+ * You can shift the cursors and print out the substring from one to the other cursorposition.
+ *
+ * Hochschule München, Fakultät 07
+ * Softwarearchitektur, IF4B
+ * @author Florian Frank, Alioun Diagne
+ * @version 29.03.2016
+ *
  */
 public class DualSplitString extends SimpleSplitString{
 
+    /**current cursorposition of the second cursor*/
     private int cursorPosition2;
 
+    /**
+     * The constructor allocates the delivered string
+     * and sets the cursors to the default values
+     * @param string delivered string
+     */
     public DualSplitString(String string){
         super(string);
         this.cursorPosition2 = string.length();
     }
 
+    /**
+     * shifts the second cursor about one step forward and returns the current object
+     * if the cursor is at the end of the string it only returns the object
+     * @return current object
+     */
     public DualSplitString forward2(){
         if(this.cursorPosition2 != this.string.length()){
             this.cursorPosition2++;
@@ -17,15 +37,27 @@ public class DualSplitString extends SimpleSplitString{
         return this;
     }
 
+    /**
+     * sets the second cursor to it's default position
+     * @return current object
+     */
     public DualSplitString home2(){
         this.cursorPosition2 = 0;
         return this;
     }
 
+    /**
+     * returns position of the second cursor
+     * @return cursorposition2
+     */
     public int getCursor2(){
         return this.cursorPosition2;
     }
 
+    /**
+     * returns the string between cursor1 and cursor2
+     * @return substring between the cursorpositions
+     */
     @Override
     public String toString(){
         if(this.cursorPosition > this.cursorPosition2){
@@ -35,6 +67,11 @@ public class DualSplitString extends SimpleSplitString{
         }
     }
 
+    /**
+     * checks if string, cursor1 and cursor2 are equal to the delviered object
+     * @param anything object to compare
+     * @return boolean if comparison is true or false
+     */
     @Override
     public boolean equals(Object anything){
         if(anything != null) {
--- SplitString/src/SimpleSplitString.java
+++ SplitString/src/SimpleSplitString.java
@@ -1,11 +1,26 @@
-/**
- * Created by florianfrank on 29.03.16.
- */
+/*
+* This class represents a simple string with a cursorPosition.
+* The defaultposition of the cursor is before the first letter of the string.
+* You can shift the cursor and print out the substring from the cursorposition to the end of the string
+*
+* Hochschule München, Fakultät 07
+* Softwarearchitektur, IF4B
+* @author Florian Frank, Alioun Diagne
+* @version 29.03.2016
+*
+**/
 public class SimpleSplitString implements SplitString {
 
+    /*The current position of the cursor*/
     protected int cursorPosition;
+    /**deliverd string*/
     protected String string;
 
+    /**
+     * constructor allocates the delivered string
+     * and sets the cursorPosition to 0
+     * @param string delivered String
+     */
     public SimpleSplitString(String string){
         if(string == null){
             throw new NullPointerException();
@@ -14,11 +29,20 @@ public class SimpleSplitString implements SplitString {
         this.cursorPosition = 0;
     }
 
+    /**
+     * returns the current cursorposition
+     * @return current cursorposition
+     */
     @Override
     public int getCursor() {
         return cursorPosition;
     }
 
+    /**
+     * shifts the cursor about one step forward and returns the current object
+     * if the cursor is at the end of the string it only returns the object
+     * @return current object
+     */
     @Override
     public SplitString forward() {
         if(this.cursorPosition != this.string.length()){
@@ -27,17 +51,30 @@ public class SimpleSplitString implements SplitString {
         return this;
     }
 
+    /**
+     * sets the cursor to the defaultposition
+     * @return current object
+     */
     @Override
     public SplitString home() {
         this.cursorPosition = 0;
         return this;
     }
 
+    /**
+     * returns a substring from the cursorposition to the end of the string
+     * @return substring from cursorposition to the end
+     */
     @Override
     public String toString(){
             return this.string.substring(this.cursorPosition,this.string.length());
     }
 
+    /**
+     * checks if string and cursorposition of the current and the delivered object are equal
+     * @param anything object to compare
+     * @return boolean if objects are equal or not
+     */
     @Override
     public boolean equals(Object anything){
         if(anything != null){
